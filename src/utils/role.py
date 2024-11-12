300462b30ff976536d9ee587cd6ec8a4f827a8ba
JimLiu <jimliu@ispi.com.tw>
2024-11-12 12:22:52 +0800

    add new Role Guest

--- src/utils/role.py
+++ src/utils/role.py
@@ -4,9 +4,10 @@ import enum
 class UserRole(enum.StrEnum):
     ADMIN = "ADMIN"
     NORMAL = "NORMAL"
+    GUEST = "GUEST"
 
 class UserRoleGroup:
-
+    PUBLIC = (UserRole.ADMIN, UserRole.NORMAL, UserRole.GUEST, )
     ALL_USER = (UserRole.ADMIN, UserRole.NORMAL,)
     ONLY_ADMIN = (UserRole.ADMIN,)
     
